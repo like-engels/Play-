3bd4124e34f45adce339ee4319164676e6df3681
Engels Tercero <higashikataengels@icloud.com>
2023-01-24 15:54:17 -0600

    Initial Commit

---
+++ Play!/Play_App.swift
@@ -0,0 +1,17 @@
+//
+//  Play_App.swift
+//  Play!
+//
+//  Created by Higashikata Maverick on 24/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Play_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
